6b2698a5ad2314f883ff9a3400e5a4bd3b3ffdc0
Noah_Dave <m.davenoa@gmail.com>
2025-03-02 23:21:11 +0300

    project structure

---
+++ src/components/ui/button.jsx
@@ -0,0 +1,12 @@
+// ui/button.js
+import React from 'react';
+
+const Button = (props) => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// filepath: /D:/Web Dev/Major Projects/DALL-E-image-generator-App/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
+    },
+  },
 })
