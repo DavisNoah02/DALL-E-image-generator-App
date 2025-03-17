f38f34ae9ae6d30324b907d1f8201ee3a184b7d6
Noah_Dave <m.davenoa@gmail.com>
2025-03-17 20:41:50 +0300

    uninstall PWA

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa';
-import dotenv from 'vite-plugin-dotenv';
+
+
 
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -13,8 +13,8 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA(),
-    dotenv(),
+    
+    
   ],
   resolve: {
     alias: {
