704ec1e816704d6f4f01a0ed440661ba2e84f9e2
Noah_Dave <m.davenoa@gmail.com>
2025-03-17 21:15:00 +0300

    .env correction

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 // filepath: /D:/Web Dev/Major Projects/DALL-E-image-generator-App/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-
-
+import tailwindcss from 'tailwindcss'
 import path from 'path'
 import { fileURLToPath } from 'url'
+
 
 // https://vite.dev/config/
 export default defineConfig({
